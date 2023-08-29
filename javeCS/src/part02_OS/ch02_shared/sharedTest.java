80aaa97087cf63616ebdfde74c327b250fc37e91
useonguk <102145499+useonguk@users.noreply.github.com>
2023-08-29 12:04:55 +0900

    Solve: deadlock(bad ex)

--- javeCS/src/part02_OS/ch02_shared/sharedTest.java
+++ javeCS/src/part02_OS/ch02_shared/sharedTest.java
@@ -1,11 +1,12 @@
 package part02_OS.ch02_shared;
 
 public class sharedTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread_r1_Wait_r2 t1 = new Thread_r1_Wait_r2();
         Thread_r2_Wait_r1 t2 = new Thread_r2_Wait_r1();
 
         t1.start();
+//        Thread.sleep(100);//잠깐 대기시켜서 해결된다(좋지 못한 방법)
         t2.start();
     }
 }
